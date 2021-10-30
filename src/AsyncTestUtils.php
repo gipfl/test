a9acaae4d49e0440206f7591f97d7e8de8e61335
Thomas Gelf <thomas@gelf.net>
2021-10-30 10:44:57 +0200

    Initial import

---
+++ src/AsyncTestUtils.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace gipfl\Stream;
+
+use React\EventLoop\Factory;
+use React\EventLoop\LoopInterface;
+
+trait AsyncTestUtils
+{
+    protected $loop;
+
+    protected function loop()
+    {
+        if ($this->loop === null) {
+            $this->loop = Factory::create();
+        }
+
+        return $this->loop;
+    }
+
+    protected function failAfterSeconds($seconds, LoopInterface $loop)
+    {
+        $loop->addTimer($seconds, function () use ($seconds) {
+            throw new \RuntimeException("Timed out after $seconds seconds");
+        });
+    }
+
+    protected function collectErrorsForNotices(&$errors)
+    {
+        \set_error_handler(function ($errno, $errstr, $errfile, $errline) use (&$errors) {
+            if (\error_reporting() === 0) { // @-operator in use
+                return false;
+            }
+            $errors[] = new \ErrorException($errstr, 0, $errno, $errfile, $errline);
+
+            return false; // Always continue with normal error processing
+        }, E_ALL | E_STRICT);
+
+        \error_reporting(E_ALL | E_STRICT);
+    }
+
+    protected function throwEventualErrors(array $errors)
+    {
+        foreach ($errors as $error) {
+            throw $error;
+        }
+    }
+}
